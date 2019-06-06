76ef31e21dc83253637a95dab2ee6a2c84077aa5
pjayneet97 <porwaljayneet97@gmail.com>
2019-06-06 10:53:59 +0530

    initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { ModalModule } from 'ngx-bootstrap';
+import { AuthComponent } from './auth/auth.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { RentalsComponent } from './home/rentals/rentals.component';
+import { AddrentalComponent } from './home/addrental/addrental.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent,
+    HomeComponent,
+    RentalsComponent,
+    AddrentalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    ModalModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/addrental/addrental.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addrental',
+  templateUrl: './addrental.component.html',
+  styleUrls: ['./addrental.component.css']
+})
+export class AddrentalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
